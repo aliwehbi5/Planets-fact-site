87def4be276e94c7534ba862ac6837180a8c9c0f
aliwehbi5 <108752304+aliwehbi5@users.noreply.github.com>
2024-05-28 19:16:13 +0300

    Update main.js

--- js-files/main.js
+++ js-files/main.js
@@ -70,6 +70,7 @@ mobilePlanetsLi.forEach((li, index) => {
       catogery.classList.remove("active");
     });
     mobileCatogries[0].classList.add("active");
+    document.body.className = li.querySelector("h2").textContent;
   });
 });
 
